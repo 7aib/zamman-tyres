831da716f2f54c857bd0d92c4febf6a581770e04
Zohaib Shamshad <zohaibrana03@gmail.com>
2024-11-21 22:43:25 +0500

    First page created

---
+++ src/Components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header()
+{
+    return(
+        <div id="main">
+            <div className="name">
+                <h2>STEP UP Your..!</h2>
+                <h1><span>VEHICLE</span> WITH US </h1>
+                <p className="details">Yar Zaman ab dekh kr bata yaha kya likhu .....?</p>
+            </div>
+            {/* <div className="header-btns">
+                <a href="#" className="header-btn">Join Us </a>
+            </div> */}
+
+        </div>
+
+    );
+
+}
+export default Header;
